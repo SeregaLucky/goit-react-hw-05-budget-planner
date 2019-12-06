eea677f34c93509b5c9bbf17348bf0cad7499292
Sergii Stryzg <strizgsergey@gmail.com>
2019-12-06 16:04:12 +0200

    Updates

---
+++ precache-manifest.9ce3db06534b7355a6b64b9bec833c94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1355d6cf6ebb2d1dbf449241568a5b",
+    "url": "/SeregaLucky/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "721d40db12bf28017ac6",
+    "url": "/SeregaLucky/goit-react-hw-05-budget-planner/static/css/main.eb52151a.chunk.css"
+  },
+  {
+    "revision": "42add05f18ed379438ed",
+    "url": "/SeregaLucky/goit-react-hw-05-budget-planner/static/js/2.0be58b8b.chunk.js"
+  },
+  {
+    "revision": "721d40db12bf28017ac6",
+    "url": "/SeregaLucky/goit-react-hw-05-budget-planner/static/js/main.e5ca5ad8.chunk.js"
+  },
+  {
+    "revision": "00338379e0c7d82040b1",
+    "url": "/SeregaLucky/goit-react-hw-05-budget-planner/static/js/runtime-main.060a5a83.js"
+  }
+]);
