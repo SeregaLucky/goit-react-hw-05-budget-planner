71802c5755760ca82e85cb5bdb1a68fb174c5980
Sergii Stryzg <strizgsergey@gmail.com>
2019-12-06 16:02:03 +0200

    finish=)

--- src/redux/planner/plannerSelectors.js
+++ src/redux/planner/plannerSelectors.js
@@ -1,16 +1,16 @@
+import { createSelector } from 'reselect';
+
 const getbudget = state => state.planner.budget;
 const getExpenses = state => state.planner.expenses;
 
-const calculateTotalExpenses = state => {
-  const expenses = getExpenses(state);
-  return expenses.reduce((total, expense) => total + expense.amount, 0);
-};
+const calculateTotalExpenses = createSelector([getExpenses], expenses =>
+  expenses.reduce((total, expense) => total + expense.amount, 0),
+);
 
-const calculateBalance = state => {
-  const budget = getbudget(state);
-  const totalExpenses = calculateTotalExpenses(state);
-  return budget - totalExpenses;
-};
+const calculateBalance = createSelector(
+  [getbudget, calculateTotalExpenses],
+  (budget, totalExpenses) => budget - totalExpenses,
+);
 
 export default {
   getbudget,
